a6e304d04bf4bfd583fcfc9504df5ccdcf908e8d
lxy <abetterlxy@hotmail.com>
2016-10-21 11:19:34 +0800

    add readme.md

--- jquery-drag.js
+++ jquery-drag.js
@@ -7,7 +7,8 @@
             var _this = $(this);
             x = e.clientX - parseFloat(_this.css('left'));
             y = e.clientY - parseFloat(_this.css('top'));
-            cb.down && cb.down();
+
+            cb.down && cb.down(_this);
 
             $(document).on('mousemove', function(e){
                 _this.css({
@@ -20,31 +21,33 @@
                 obj.left = parseFloat(_this.css('left'));
                 obj.top = parseFloat(_this.css('top'));
                 obj.box = _this.parent('.container');
+
                 cb.move && cb.move(obj);
-                cb.range && range();
-                function range() {
-                    if(obj.left > obj.box.width() - obj.ele.width()) {
-                        obj.ele.css('left', obj.box.width() - obj.ele.width());
-                    }
-                    if(obj.left < 0) {
-                        obj.ele.css('left', 0);
-                    }
-                    if(obj.top > obj.box.height() - obj.ele.height()) {
-                        obj.ele.css('top', obj.box.height() - obj.ele.height());
-                    }
-                    if(obj.top < 0) {
-                        obj.ele.css('top', 0);
-                    }
-                }
+                cb.range && range(obj);
+
             });
 
             $(document).on('mouseup', function(e){
-                var obj = {};
-                obj.ele = _this;
                 $(document).off('mousemove');
                 $(document).off('mouseup');
-                cb.up && cb.up(obj);
+
+                cb.up && cb.up(_this);
             });
         });
+        
+        function range(obj) {
+            if(obj.left > obj.box.width() - obj.ele.width()) {
+                obj.ele.css('left', obj.box.width() - obj.ele.width());
+            }
+            if(obj.left < 0) {
+                obj.ele.css('left', 0);
+            }
+            if(obj.top > obj.box.height() - obj.ele.height()) {
+                obj.ele.css('top', obj.box.height() - obj.ele.height());
+            }
+            if(obj.top < 0) {
+                obj.ele.css('top', 0);
+            }
+        }
     }
 })(jQuery);
---
+++ readme.md
@@ -0,0 +1,42 @@
+
+$(ele).drag(obj):
+
+obj: 可选，传递一个对象，可分别触发drag事件的回调
+
+obj.down: 按下元素时触发
+
+obj.move: 拖动元素时触发
+
+obj.up: 松开元素时触发
+
+obj.range: 是否限制元素拖拽范围
+
+exm:
+var obj = {
+
+    // onmousedown触发时执行
+    // ele: 元素
+    down: function(ele) {
+        console.log('move');
+    }
+
+    // onmousemove触发时执行
+    // obj: object
+    // obj.ele: 元素
+    // obj.e: 事件对象
+    // obj.box: 元素的的父节点
+    // obj.left: 元素的实时left
+    // obj.top: 元素的实时top
+    move: function(obj) {   
+        console.log('move');
+    }
+
+    // onmouseup触发时执行
+    // ele: 元素本身
+    up: function(ele) {
+        console.log(ele.attr('class'));
+    }
+
+    range: true // 限制拖拽范围
+}
+$(ele).drag(obj);
