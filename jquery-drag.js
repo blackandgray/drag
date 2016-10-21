7972f453d19c7ff1949b522e2b77ab25b5a0541a
lxy <abetterlxy@hotmail.com>
2016-10-21 11:05:32 +0800

    'change'

---
+++ jquery-drag.js
@@ -0,0 +1,50 @@
+(function($){
+    $.fn.drag = function(cb) {
+        var x = 0,
+            y = 0;
+        this.css('position', 'absolute');
+        this.on('mousedown', function(e){
+            var _this = $(this);
+            x = e.clientX - parseFloat(_this.css('left'));
+            y = e.clientY - parseFloat(_this.css('top'));
+            cb.down && cb.down();
+
+            $(document).on('mousemove', function(e){
+                _this.css({
+                    'left': e.clientX - x,
+                    'top': e.clientY - y
+                });
+                var obj = {};
+                obj.e = e;
+                obj.ele = _this;
+                obj.left = parseFloat(_this.css('left'));
+                obj.top = parseFloat(_this.css('top'));
+                obj.box = _this.parent('.container');
+                cb.move && cb.move(obj);
+                cb.range && range();
+                function range() {
+                    if(obj.left > obj.box.width() - obj.ele.width()) {
+                        obj.ele.css('left', obj.box.width() - obj.ele.width());
+                    }
+                    if(obj.left < 0) {
+                        obj.ele.css('left', 0);
+                    }
+                    if(obj.top > obj.box.height() - obj.ele.height()) {
+                        obj.ele.css('top', obj.box.height() - obj.ele.height());
+                    }
+                    if(obj.top < 0) {
+                        obj.ele.css('top', 0);
+                    }
+                }
+            });
+
+            $(document).on('mouseup', function(e){
+                var obj = {};
+                obj.ele = _this;
+                $(document).off('mousemove');
+                $(document).off('mouseup');
+                cb.up && cb.up(obj);
+            });
+        });
+    }
+})(jQuery);
